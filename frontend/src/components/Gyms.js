5002759b277d814ff764f26ff3de7e94efaeb5c4
alexandramarum <alexandramarum@gmail.com>
2024-12-01 21:26:44 -0500

    basic ui

--- frontend/src/components/Gyms.js
+++ frontend/src/components/Gyms.js
@@ -1,7 +1,41 @@
 import React from "react";
 
 function Gyms() {
-  return <h1>Welcome to the Gyms Page</h1>;
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-8">
+      <h3 className="text-2xl font-bold mb-6 text-gray-700">
+            Gyms
+      </h3>
+          <form className="space-y-6">
+            <div>
+              <label className="block text-sm font-medium text-gray-700 mb-1">
+                Longitude
+              </label>
+              <input
+                type="text"
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+
+            <div>
+              <label className="block text-sm font-medium text-gray-700 mb-1">
+                Latitude
+              </label>
+              <input
+                type="text"
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="w-full bg-blue-600 text-white font-bold py-2 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+            >
+              Find Gyms Nearby
+            </button>
+          </form>
+        </div>
+  )
 }
 
 export default Gyms;
