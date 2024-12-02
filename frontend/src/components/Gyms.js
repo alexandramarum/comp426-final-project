1a5f3911c742dc71aa57830f09f980584c10f34a
alexandramarum <alexandramarum@gmail.com>
2024-12-01 22:37:34 -0500

    add error message

--- frontend/src/components/Gyms.js
+++ frontend/src/components/Gyms.js
@@ -4,27 +4,35 @@ const Gyms = () => {
 
   const [latitude, setLatitude] = useState("");
   const [longitude, setLongitude] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function (position) {
-          setLatitude(position.coords.latitude)
-          setLongitude(position.coords.longitude)
-      }, function (error) {
-          console.error("Error getting location:", error);
-          alert("Could not get your location. Please allow location access to use the live chat.");
-      });
-  } else {
-      alert("Geolocation is not supported by your browser.");
-  }
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function (position) {
+              setLatitude(position.coords.latitude)
+              setLongitude(position.coords.longitude)
+          });
+      } else {
+          alert("Geolocation is not supported by your browser.");
+      }
   })
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    if (!latitude || !longitude) {
+      setErrorMessage("Location data is missing. Please allow location access to proceed.")
+    } else {
+      setErrorMessage("")
+    }
+  };
 
   return (
     <div className="bg-white shadow-lg rounded-lg p-8">
       <h3 className="text-2xl font-bold mb-6 text-gray-700">
             Gyms
       </h3>
-          <form className="space-y-6">
+          <form className="space-y-6" onSubmit={onSubmit}>
             <div>
               <label className="block text-sm font-medium text-gray-700 mb-1">
                 Longitude
@@ -55,6 +63,11 @@ const Gyms = () => {
             >
               Find Gyms Nearby
             </button>
+            {errorMessage && (
+          <div className="mt-4 text-red-600">
+            {errorMessage}
+          </div>
+        )}
           </form>
         </div>
   )
